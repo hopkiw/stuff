5ad4ca85e4e348768612b73af1570fa50986afed
Liam Hopkins <liamh@google.com>
2020-09-02 11:14:04 -0700

    add rps script

---
+++ code/examples/python/rock_paper_scissors.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import random
+import time
+
+options = ["rock", "paper", "scissors"]
+seeded = False
+
+def generate_play_string():
+    string = ""
+    for num in range(len(options)):
+        string += options[num].capitalize()
+        if num != len(options):
+            string += ", "
+    return string + ": "
+
+def get_computer_choice():
+    global seeded
+    if not seeded:
+        random.seed(time.time())
+        seeded = True
+    return random.randint(0,2)
+
+def play_again():
+    choice = raw_input("Play again? Y/n ").lower()
+    return (choice == "y" or choice == "")
+
+def main():
+    play_string = generate_play_string()
+    while True:
+        player = raw_input(play_string).lower()
+        if player not in options:
+            print("Hey, that's not one of the options!")
+            continue
+        player = options.index(player)
+        computer = get_computer_choice()
+
+        if player == computer:
+            print("It's a draw! We both picked %s" % options[player])
+        elif computer == player+1 or (computer == 0 and player == len(options)-1):
+            print("Computer wins! %s beats %s" % (options[computer], options[player]))
+        else:
+            print("Player wins! %s beats %s" % (options[player], options[computer]))
+
+        if not play_again():
+            break
+
+if __name__ == "__main__":
+    main()
