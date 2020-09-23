9d6fa766320582fd6ac1b0b65abd67b95fd223b3
Corey McGrath <coreymcgrath@MacBook-Pro-2.lan1>
2020-09-22 23:54:37 -0400

    Added stuff to address book program

--- address_book.py
+++ address_book.py
@@ -27,9 +27,19 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--name',help='The name of the entry to add.')
 	parser.add_argument('--email',help='The email address of the entry to add.')
+	parser.add_argument('--search',help='The term to search for.')
 	args = parser.parse_args()
-	new_entry = Entry(args.name,args.email)
-	entries.append(new_entry)
+	if args.name or args.email:
+		new_entry = Entry(args.name,args.email)
+		entries.append(new_entry)
+		print(new_entry.info())
+	if args.search:
+		found = []
+		for entry in entries:
+			if args.search in entry.name or args.search in entry.email:
+				found.append(entry)
+		for entry in found:
+			entry.info()
 
 if __name__ == "__main__":
 	main()
