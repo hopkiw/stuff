0b2f84cc0365b45499a8018e45418cf55cc10164
Corey McGrath <coreymcgrath@MacBook-Pro-2.lan1>
2020-10-06 19:57:56 -0400

    Made modifications to address book

--- address_book.py
+++ address_book.py
@@ -20,18 +20,18 @@ entries = []
 
 
 class Entry(object):
-  def __init__(self, name, email, address):
+  def __init__(self, name, email='', address='', title='', first_name='', middle_name='', last_name='', workplace='', phone=''):
     self.name = name
     self.email = email
+    self.address = address
+    self.title = title
+    self.first_name = first_name
+    self.middle_name = middle_name
+    self.last_name = last_name
+    self.workplace = workplace
+    self.phone = phone
+    
     # Future fields:
-    # Prefix, title
-    # First name
-    # Middle name
-    # Last, family name
-    # Workplace
-    # Phone number, with type
-    # Email address
-    # Address
     # Instant messenger handle, with service
     # Important dates/Anniversaries
     # Notes
@@ -51,6 +51,17 @@ class Entry(object):
   def __repr__(self):
     return self.__str__()
 
+  def info(self):
+        if self.first_name and self.last_name:
+            if self.title:
+                print(self.title + ' ' + self.first_name + ' ' + self.last_name)
+            else:
+                print(self.first_name + ' ' + self.last_name)
+        else:
+            print(self.name)
+        print('Email: ' + self.email)
+        print('Phone: ' + self.phone)
+
 
 def main():
   parser = argparse.ArgumentParser()
@@ -58,6 +69,12 @@ def main():
   parser.add_argument('--name')
   parser.add_argument('--email')
   parser.add_argument('--address')
+  parser.add_argument('--title')
+  parser.add_argument('--first_name')
+  parser.add_argument('--middle_name')
+  parser.add_argument('--last_name')
+  parser.add_argument('--workplace')
+  parser.add_argument('--phone')
   args = parser.parse_args()
 
   # filter: Return a sequence yielding those items of iterable for which
@@ -67,9 +84,8 @@ def main():
     parser.error('Must supply at least one attribute.')
 
   # Pre-seed the entries list with a fake entry.
-  entries.append(Entry(name="Fake name", email="fake@email.gov",
-                       address="123 fake street"))
-
+  entries.append(Entry(name="Fake name", title='Mr.', first_name='Fakest', last_name='Name', email="fake@email.gov",
+                       address="123 fake street", phone='1-800-FAKE-NUMB'))
   if args.action == 'add':
     entry_args = args.__dict__
     entry_args.pop('action')
@@ -77,15 +93,45 @@ def main():
   elif args.action == 'delete':
     for idx in range(len(entries)):
       entry = entries[idx]
-      # TODO: If user specifies two fields, only delete those that match both.
-      if (entry.name == args.name or entry.email == args.email or
-          entry.address == args.address):
-        entries.pop(idx)
+      if args.name and entry.name != args.name:
+        break
+      elif args.email and entry.email != args.email:
+        break
+      elif args.address and entry.address != args.address:
+        break
+      elif args.title and entry.title != args.title:
+        break
+      elif args.first_name and entry.first_name != args.first_name:
+        break
+      elif args.middle_name and entry.middle_name != args.middle_name:
+        break
+      elif args.last_name and entry.last_name != args.last_name:
+        break
+      elif args.workplace and entry.workplace != args.workplace:
+        break
+      elif args.phone and entry.phone != args.phone:
+        break
+      entries.pop(idx)
   elif args.action == 'search':
     for entry in entries:
-      if (entry.name == args.name or entry.email == args.email or
-          entry.address == args.address):
-        print(entry)
+      if args.name and entry.name == args.name:
+        entry.info()
+      elif args.email and entry.email == args.email:
+        entry.info()
+      elif args.address and entry.address == args.address:
+        entry.info()
+      elif args.title and entry.title == args.title:
+        entry.info()
+      elif args.first_name and entry.first_name == args.first_name:
+        entry.info()
+      elif args.middle_name and entry.middle_name == args.middle_name:
+        entry.info()
+      elif args.last_name and entry.last_name == args.last_name:
+        entry.info()
+      elif args.workplace and entry.workplace == args.workplace:
+        entry.info()
+      elif args.phone and entry.phone == args.phone:
+        entry.info()
 
 
 if __name__ == '__main__':
