0272aed4355965d26766cdf9c8de51a21409a682
Liam Hopkins <we.hopkins@gmail.com>
2025-07-25 21:13:18 -0700

    update emby browser

--- code/emby_browser.py
+++ code/emby_browser.py
@@ -4,54 +4,78 @@ import urwid
 from emby_tool import get_views, get_items
 
 
-def menu(title, choices_):
-    body = [urwid.Text(title), urwid.Divider()]
-    for c in choices_:
-        button = urwid.Button(c['Name'])
-        urwid.connect_signal(button, 'click', item_chosen, c)
-        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+class EmbyBrowser:
+    palette = [
+        ("body", "black", "light gray"),
+        ("flagged", "black", "dark green", ("bold", "underline")),
+        ("focus", "light gray", "dark blue", "standout"),
+        ("flagged focus", "yellow", "dark cyan", ("bold", "standout", "underline")),
+        ("head", "yellow", "black", "standout"),
+        ("foot", "light gray", "black"),
+        ("key", "light cyan", "black", "underline"),
+        ("title", "white", "black", "bold"),
+        ("dirmark", "black", "dark cyan", "bold"),
+        ("flag", "dark gray", "light gray"),
+        ("error", "dark red", "light gray"),
+    ]
 
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+    def __init__(self):
+        self.view = None
+        self.show_views()
+
+    def show_views(self):
+        views = get_views()
+        body = []
+        for view in views['Items']:
+            button = urwid.Button(view['Name'])
+            urwid.connect_signal(button, 'click', self.show_items, view['Id'])
+            body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+        self.listbox = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+        frame = urwid.Frame(
+            urwid.AttrMap(
+                urwid.ScrollBar(
+                    self.listbox,
+                ),
+                "body",
+            ),
+        )
+        if self.view:
+            self.view.body = frame
+        else:
+            self.view = frame
 
 
-def item_chosen(button, choice):
-    body = []
-    items = get_items(choice['Id'])
-    for item in items['Items']:
-        button = urwid.Button(item['Name'])
-        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-    boxlist = urwid.ListBox(urwid.SimpleFocusListWalker(body))
-    main_win.original_widget = urwid.Padding(boxlist, left=2, right=2)
+    def show_items(self, w, parent_id):
+        items = get_items(parent_id)
+        body = []
+        for item in items['Items']:
+            button = urwid.Text(item['Name'])
+            body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+
+        self.listbox = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+        self.view.body = urwid.AttrMap(
+                urwid.ScrollBar( self.listbox,), "body",
+        )
 
 
-main_win = None
+
+    def main(self):
+        """Run the program."""
+        self.loop = urwid.MainLoop(self.view, self.palette, unhandled_input=self.unhandled_input)
+        self.loop.run()
 
 
-def exit_program(button):
-    raise urwid.ExitMainLoop()
+    def unhandled_input(self, k):
+        # update display of focus directory
+        if k in {"q", "Q"}:
+            raise urwid.ExitMainLoop()
+        if k == 'left':
+            # heck yea
+            self.show_views()
 
 
 def main():
-    global main_win
-    # choices = ["Chapman", "Cleese", "Gilliam", "Idle", "Jones", "Palin"]
-    views = get_views()
-    choices = []
-    for view in views['Items']:
-        entry = {'Name': view['Name'], 'Id': view['Id']}
-        choices.append(entry)
-    boxlist = menu('Views', choices)
-    main_win = urwid.Padding(boxlist, left=2, right=2)
-    top = urwid.Overlay(
-        main_win,
-        urwid.SolidFill("\N{MEDIUM SHADE}"),
-        align=urwid.CENTER,
-        width=(urwid.RELATIVE, 60),
-        valign=urwid.MIDDLE,
-        height=(urwid.RELATIVE, 60),
-        min_width=20,
-        min_height=9,
-    )
-    urwid.MainLoop(top, palette=[("reversed", "standout", "")]).run()
+    EmbyBrowser().main()
 
 
 if __name__ == '__main__':
