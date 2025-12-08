c2542d49a63f9e0c22a1bc8cf272c0336132dd60
Liam Hopkins <we.hopkins@gmail.com>
2025-12-07 19:52:22 -0800

    finish starfield

--- code/sdl_starfield/starfield.cpp
+++ code/sdl_starfield/starfield.cpp
@@ -77,6 +77,7 @@ int main() {
     if (!starfield::Init())
         return 1;
 
+    /*
     const SDL_Color colors[] = {
         {0xFF, 0x00, 0x00, 0xFF},
         {0x00, 0xFF, 0x00, 0xFF},
@@ -84,6 +85,7 @@ int main() {
         {0xFF, 0xFF, 0x00, 0xFF},
         {0x00, 0xFF, 0xFF, 0xFF},
     };
+    */
 
     const SDL_Color bgColor{0x0, 0x0, 0x0, 0xFF};
     const Point center = {starfield::SCREEN_WIDTH / 2, starfield::SCREEN_HEIGHT / 2};
@@ -91,23 +93,14 @@ int main() {
 
     std::vector<Star> stars;
     float circleAngle = 0.0;
-    for (int i = 0; i < 15; ++i) {
+    for (int i = 0; i < 75; ++i) {
         Point p = {rand() % starfield::SCREEN_WIDTH, rand() % starfield::SCREEN_HEIGHT};
-        /*
-        Point p = {
-            center.x + static_cast<int>(140 * sin(circleAngle)),
-            center.y - static_cast<int>(140 * cos(circleAngle))
-        };
-        */
-        Star s = {p, 8, 0.08};
+        Star s = {p, rand() % 8, 0.08};
         stars.push_back(s);
         circleAngle += 0.5;
     }
 
     Uint32 lastTicks = SDL_GetTicks();
-
-    bool pause = true;
-    bool print = false;
     bool quit = false;
     while (!quit) {
         SDL_Event e;
@@ -121,18 +114,22 @@ int main() {
                         case 'q':
                             quit = true;
                             break;
-                        case SDLK_SPACE:
-                            pause = !pause;
-                            break;
                         case 'j':
                             std::cout << "print true" << std::endl;
-                            print = true;
                             break;
                     }
                     break;
                 case SDL_MOUSEBUTTONDOWN:
                     if (e.motion.state & SDL_BUTTON_LMASK) {
-                        std::cout << "clicked at " << Point{e.motion.x, e.motion.y} << std::endl;
+                        // std::cout << "clicked at " << Point{e.motion.x, e.motion.y} << std::endl;
+                        for (auto it = stars.begin(); it != stars.end(); ++it) {
+                            SDL_Rect r = {it->p.x, it->p.y, it->size, it->size};
+                            SDL_Point p = {e.motion.x, e.motion.y};
+                            if (SDL_PointInRect(&p, &r) == SDL_TRUE) {
+                                std::cout << "clicked on rect at " << Point{p.x, p.y} << " with velocity "
+                                    << it->vel << std::endl;
+                            }
+                        }
                     }
                     break;
             }
@@ -146,21 +143,8 @@ int main() {
 
         auto idx = 0;
         for (auto it = stars.begin(); it != stars.end(); ++it) {
-            if (it->p.x < 0 || it->p.x > starfield::SCREEN_WIDTH) {
-                if (print)
-                    std::cout << "x out of bounds: " << it->p.x << std::endl;
-                // continue;
-                it->p = {rand() % starfield::SCREEN_WIDTH, rand() % starfield::SCREEN_HEIGHT};
-            }
-
-            if (it->p.y < 0 || it->p.y > starfield::SCREEN_HEIGHT) {
-                if (print)
-                    std::cout << "y out of bounds: " << it->p.y << std::endl;
-                // continue;
-                it->p = {rand() % starfield::SCREEN_WIDTH, rand() % starfield::SCREEN_HEIGHT};
-            }
-
-            SDL_Color c = colors[idx];
+            // SDL_Color c = colors[idx];
+            SDL_Color c = {0xFF, 0xFF, 0xFF, 0xFF};
             SDL_Rect r = {it->p.x, it->p.y, it->size, it->size};
             SDL_SetRenderDrawColor(starfield::renderer, c.r, c.g, c.b, c.a);
             SDL_RenderFillRect(starfield::renderer, &r);
@@ -171,27 +155,13 @@ int main() {
             float width = it->p.x - center.x;
             float height = it->p.y - center.y;
 
-            if (width == 0 || height == 0)
-                continue;
-
-            if (print) {
-                std::cout << "test star " << it->p << std::endl;
-                std::cout << "width: " << width << " height: " << height << std::endl;
-            }
-
             float distance = sqrt((width * width) + (height * height));
             distance += it->vel * delta;
+            it->vel += 0.001 * delta;
 
             float angle = atan(height / width);
             float newWidth = distance * cos(angle);
             float newHeight = distance * sin(angle);
-
-            if (print) {
-                std::cout << "new distance to star: " << distance << std::endl;
-                std::cout << "at angle: " << angle << std::endl;
-                std::cout << "newWidth: " << newWidth << std::endl;
-                std::cout << "newHeight: " << newHeight << std::endl;
-            }
 
             if (it->p.x > center.x)
                 it->p.x = center.x + abs(newWidth);
@@ -203,20 +173,16 @@ int main() {
             else
                 it->p.y = center.y - abs(newHeight);
 
-            /*
-            if (print)
-                std::cout << "next star at " << Point{r.x, r.y} << std::endl;
+            if (it->p.x < 0 || it->p.x > starfield::SCREEN_WIDTH) {
+                it->p = {rand() % starfield::SCREEN_WIDTH, rand() % starfield::SCREEN_HEIGHT};
+                it->vel = (((rand() % 4) + 4.0) / 100.0);
+            }
 
-            SDL_SetRenderDrawColor(starfield::renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-            SDL_RenderFillRect(starfield::renderer, &r);
-
-            SDL_SetRenderDrawColor(starfield::renderer, 0xFF, 0x0, 0x0, 0xFF);
-            SDL_RenderDrawLine(starfield::renderer, it->p.x, it->p.y, r.x, r.y);
-            */
+            if (it->p.y < 0 || it->p.y > starfield::SCREEN_HEIGHT) {
+                it->p = {rand() % starfield::SCREEN_WIDTH, rand() % starfield::SCREEN_HEIGHT};
+                it->vel = (((rand() % 4) + 4.0) / 100.0);
+            }
         }
-
-        print = false;
-
         SDL_RenderPresent(starfield::renderer);
         lastTicks = currentTicks;
     }
